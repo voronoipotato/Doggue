ff957d94fe4db3a92c5b4032fdfa954f8e71c58b
Alan Ball <vash.arclan@gmail.com>
2020-02-29 00:18:56 -0500

    Trying to mimic Elmish.React

--- Program.fs
+++ Program.fs
@@ -2,43 +2,43 @@
 
 open System
 open Elmish
-
+module Terminal = 
+  type Msg = | ChangeValue of string
+  type Element = Dialog of string * ( string -> unit ) | Rogue of char * (char -> unit)
 module Program =
-  let withConsole program: Program<_,_,_,_> =
+  let withTerminal program: Program<_,_,_,_> =
     let setState model dispatch = 
-      let (s: string) = (Program.view program) model dispatch
-      Console.WriteLine(s)
+      let el = (Program.view program) model dispatch
+      match el with
+      | Terminal.Dialog (d, f) -> 
+        Console.WriteLine(d)
+        Console.ReadLine() |> f
+      | Terminal.Rogue (c, f) -> Console.ReadKey(true).KeyChar  |> f
+      
       ()
     program |> Program.withSetState setState
 
 
 type Model = { Value : string }
 
-type Msg = | ChangeValue of string
-let init () = { Value = "" }, Cmd.none
+//type Msg = | ChangeValue of string
+let init () = { Value = "no way" }, Cmd.none
 
-let update (msg:Msg) (model: Model) =
+let update (msg:Terminal.Msg) (model: Model) =
   match msg with
-  | ChangeValue newValue ->
+  | Terminal.ChangeValue newValue ->
     { Value = newValue}, Cmd.none
 
 let view model dispatch =
-  ChangeValue "test" |> dispatch
-  sprintf "Wow! %s" model.Value
+  //(Console.ReadKey(true)).KeyChar |> string |> Terminal.ChangeValue |> dispatch
+  let s = (sprintf "Wow! %s" model.Value)
+  Terminal.Dialog ( s , Terminal.ChangeValue >> dispatch )// (fun i -> dispatch (Terminal.ChangeValue i)))
+
 
 [<EntryPoint>]
 let main argv =
   Program.mkProgram init update view
-  //|> Program.withConsoleTrace
-  |> Program.withConsole
+  |> Program.withTerminal
   |> Program.run
-
   
   0
-    // let model = {Player= {Name = ""}; Equipment= [] }
-    
-
-    // let inline k x _ = x 
-    // let f input model = model
-    // repl 0<s> model f
-    // 0 // return an integer exit code
