edd91b01991e4c935ec14493b869edd3aa3ff4a7
Alan Ball <vash.arclan@gmail.com>
2020-03-01 18:49:41 -0500

    broke out game specific code and general purpose.

--- Program.fs
+++ Program.fs
@@ -3,41 +3,86 @@
 open System
 open Elmish
 module Terminal = 
-  type Msg = | ChangeValue of string
-  type Event = OnString of (string -> unit) | OnChar of (char -> unit)
-  
-  type Element = Dialog of string * Event | Rogue of string * (char -> unit)
+  type Msg = | TextInput of string | GameInput of char
+  type OnString = (string -> unit)
+  type OnChar = (char -> unit)
+  type Width = int
+  type Length = int
+  type Position = {x: int; y: int}
+  type Dimensions = Width * Length
+
+  type Item = char * Position
+  type Header = string
+  type Element =  | Screen of Header * Element
+                  | Dialog of Header * OnString
+                  | CharInput of OnChar
+
+
+
+
 module Program =
   let withTerminal program: Program<_,_,_,_> =
-    let setState model dispatch = 
+    let  setState model dispatch = 
       let el = (Program.view program) model dispatch
-      match el with
-      | Terminal.Dialog (t, Terminal.OnString f) -> 
-        Console.WriteLine(t)
-        Console.ReadLine() |> f
-      | Terminal.Dialog (t, Terminal.OnChar f) -> 
-        Console.WriteLine(t)
-        Console.ReadKey(true).KeyChar |> f
-      | Terminal.Rogue (t, f) -> Console.ReadKey(true).KeyChar  |> f
-      
+      let rec setState' el = 
+        match el with
+        | Terminal.Dialog (t, f) -> 
+          Console.WriteLine(t)
+          Console.ReadLine() |> f
+        | Terminal.CharInput (f) -> 
+          Console.ReadKey(true).KeyChar  |> f
+        | Terminal.Screen (s, el) -> 
+          Console.WriteLine(s)
+          setState' el
+      setState' el
       ()
     program |> Program.withSetState setState
 
+module Game = 
+  type Width = int 
+  type Length = int
+  type Position = {x: int; y: int}
+  type Dimensions = Width * Length
+  type Entity =   | Rogue of char
+                  | Item of char
+                  | Room of Dimensions * Entity
+  type Model = { Name : string; Player : Position; Inventory: char list }
 
-type Model = { Value : string }
+  let update (msg:Terminal.Msg) (model: Model) =
+    match msg with
+    | Terminal.TextInput newValue ->
+      {model with Name = newValue}, Cmd.none
+    | Terminal.GameInput c ->
+      match c with 
+      | 'w' -> {model with Player = {model.Player with y = model.Player.y + 1} } , Cmd.none
+      | 'a' -> {model with Player = {model.Player with x = model.Player.x - 1} }, Cmd.none
+      | 's' -> {model with Player = {model.Player with y = model.Player.y - 1} }, Cmd.none
+      | 'd' -> {model with Player = {model.Player with x = model.Player.x + 1} }, Cmd.none
+      | 'e' -> {model with Inventory = 'a' :: model.Inventory }, Cmd.none
+      | _ -> model , Cmd.none
 
-//type Msg = | ChangeValue of string
-let init () = { Value = "no way" }, Cmd.none
+//Begin game
+type Msg = | TextInput of string | Move of char | Interact
+type Model = Game.Model
+let init () = { Model.Name = "bork bork"; 
+                Model.Player = {x = 0; y = 0}; 
+                Model.Inventory = [] }, Cmd.none
+let update = Game.update
 
-let update (msg:Terminal.Msg) (model: Model) =
-  match msg with
-  | Terminal.ChangeValue newValue ->
-    { Value = newValue}, Cmd.none
+// let update msg model = 
+//   match msg with
+//   | TextInput s -> (Game.update (Terminal.TextInput s) model)
+//   | Move c -> (Game.update (Terminal.GameInput c) model)
+//   | Interact -> (Game.update (Terminal.GameInput 'e') model)
 
 let view model dispatch =
+
   //(Console.ReadKey(true)).KeyChar |> string |> Terminal.ChangeValue |> dispatch
-  let s = (sprintf "Wow! %s" model.Value)
-  Terminal.Dialog ( s , Terminal.OnString (Terminal.ChangeValue >> dispatch) )// (fun i -> dispatch (Terminal.ChangeValue i)))
+  let doggo =  Game.Rogue 'd' 
+  let gameInput = Terminal.CharInput (Terminal.GameInput >> dispatch)
+  let bedroom = Game.Room
+  let s = (sprintf "Player Position %A" model)
+  Terminal.Screen (s, gameInput)
 
 
 [<EntryPoint>]
